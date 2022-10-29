0213c2a3ce595bdcfe0b414c1dd0c20df2489614
Angelo Vinicius <avbtavares@gmail.com>
2022-10-29 18:20:28 -0300

    ajuste

--- TesteCursoSenai_(game-mania).java
+++ TesteCursoSenai_(game-mania).java
@@ -8,7 +8,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TesteCursoSenai {
+public class TesteGameMania {
 	private WebDriver driver;
 	
 	@Before
